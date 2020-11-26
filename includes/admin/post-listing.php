975c867d51513817f017e4dd1152de1179e1a859
Ron Boutilier <ronbout@yahoo.com>
2020-11-25 21:25:39 -0600

    Added the ability to sort by venue(s) in the Posts and Products admin listing pages.

--- includes/admin/post-listing.php
+++ includes/admin/post-listing.php
@@ -1,8 +1,12 @@
 <?php 
 /**
  *  post-listing.php 
- *  add venue column to post listing 
+ *  add venues column to post listing 
  *  as well as bulk edit capability
+ * 	and sort by venues
+ * 
+ *  Author: Ron Boutilier
+ *  Date: 11/23/2020
  * 
  */
 
@@ -21,7 +25,7 @@ function taste_venues_column_posts( $column, $post_id ) {
 	global $wpdb; 
   // Venue column
   if ( 'venues' === $column ) {
-		$venue_names = 'Not assigned';
+		$venue_names = '-- Not assigned --';
 		// get the venue from the venue_products table
 		$venue_rows = $wpdb->get_results($wpdb->prepare("
 			SELECT v.name 
@@ -64,3 +68,26 @@ function taste_venues_posts_bulk_edit_input( $column_name, $post_type ) {
 	<?php
 }
 add_action( 'bulk_edit_custom_box', 'taste_venues_posts_bulk_edit_input', 10, 2 );
+
+// sorting code
+function taste_venues_posts_sortable_setup($sort_columns) {
+	$sort_columns['venues'] = 'venues';
+	return $sort_columns;
+}
+add_action( 'manage_edit-post_sortable_columns', 'taste_venues_posts_sortable_setup');
+
+// have to define behavior through 
+function taste_venues_posts_sortable_using_posts_clauses($clauses, $query) {
+	if ( $query->is_main_query() && ( 'venues' === $query->get( 'orderby' )) ) {
+		// Get the order query variable - ASC or DESC, make ASC default
+		$order = strtoupper( $query->get( 'order' ) );
+		$order = 'DESC' === $order ? 'DESC' : 'ASC';
+
+		$clauses['join'] .= "LEFT JOIN wp_taste_venues_posts vps ON wp_posts.id = vps.post_id
+												 LEFT JOIN wp_taste_venue venue ON venue.venue_id = vps.venue_id";
+		$clauses['groupby'] = "wp_posts.id";
+		$clauses['orderby'] = "COALESCE( GROUP_CONCAT( venue.name SEPARATOR ', ' ), '-----') $order";
+	}
+	return $clauses;
+}
+add_filter( 'posts_clauses', 'taste_venues_posts_sortable_using_posts_clauses', 10, 2);
