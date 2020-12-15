d71498d616ebaaf3ee8e99d11c5ae4dc2bb81f77
Ron Boutilier <ronbout@yahoo.com>
2020-12-15 00:45:50 -0600

    fix issue with venue-head.php partial template to make sure that it includes the full jquery instead of the slim version.  commented out script call to a js file that does not exist: thetaste-dashboard.js

--- page-templates/partials/venue-head.php
+++ page-templates/partials/venue-head.php
@@ -24,11 +24,14 @@ defined('ABSPATH') or die('Direct script access disallowed.');
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script
+  src="https://code.jquery.com/jquery-3.5.1.min.js"
+  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+  crossorigin="anonymous"></script>
 <script	script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
 <link rel="stylesheet" href="<?php echo TASTE_PLUGIN_INCLUDES_URL ?>/css/thetaste-venue.css">
-<script src="<?php echo TASTE_PLUGIN_INCLUDES_URL?>/js/thetaste-dashboard.js"></script>
+<!--<script src="<?php echo TASTE_PLUGIN_INCLUDES_URL?>/js/thetaste-dashboard.js"></script>-->
 <title><?php _e(the_title()); ?></title>
 </head>
