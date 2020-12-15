af61566b22f86152f2ced4d887154f065a26950f
Ron Boutilier <ronbout@yahoo.com>
2020-12-15 03:18:40 -0600

    Add the <td> for the payment id into the make payment ajax call of the campaign manager so that it gets returned to javascript to add to the end of the table.

--- includes/ajax/make_payment.php
+++ includes/ajax/make_payment.php
@@ -34,6 +34,7 @@ function make_payment_update($map_amount, $product_info, $venue_info) {
 	", $payment_id), ARRAY_A);
 	$payment_line = "
 		<tr>
+			<td>$payment_id</td>
 			<td>{$pay_row[0]['timestamp']}</td>
 			<td>$currency " . num_display($map_amount) . "</td>
 		</tr>
