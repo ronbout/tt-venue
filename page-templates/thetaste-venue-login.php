a3c35fb54106e9926de3e7f4cb368d6422827734
Ron Boutilier <ronbout@yahoo.com>
2020-12-21 14:20:46 -0600

    Added login code to the new login form in thetaste-venue-login.php.  Cleaned up a css height issue in the login css.

--- page-templates/thetaste-venue-login.php
+++ page-templates/thetaste-venue-login.php
@@ -65,36 +65,7 @@
 				<div class="bg-white shadow-lg col-sm-8 login_div text-center">
 				<i class="fas fa-user"></i>
 					<?php
-//                        $login_args = array(
-//                            "label_username" => "Username or Email: ",
-//                            "label_password" => "Password: ",
-//                            "remember" => false,
-//                        );
-//                        wp_login_form($login_args);
-
-                        function login_venue_form(){
-                            $form = '
-                                    <form name="loginform" id="loginform" action="#" method="post">
-                                        <div class="form-group row">
-                                            <label class="user_login" for="user_login">Username or Email:</label>
-                                            <input type="text" name="log" id="user_login" class="input form-control shadow-lg" value="" size="20" autofocus placeholder="Enter username or email" required/>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="user_pass" for="user_pass">Password:</label>
-                                            <input type="password" name="pwd" id="user_pass" class="input form-control shadow-lg" value="" size="20" placeholder="Enter your password" required/>
-                                        </div>
-                                        <div class="form-group row login-submit d-flex justify-content-center">
-                                            <input type="submit" name="wp-submit" id="wp-submit" class="btn button-primary py-2 px-5" value="Log In" />
-                                            <input type="hidden" name="redirect_to" value="" />
-                                        </div>
-                                    </form>';
-
-                           echo $form;
-                        }
-
-                        login_venue_form();
-					?>
-					<?php
+						login_venue_form();
 						if (get_query_var('login') === 'failed') {
 							?>
 							<div class="login-error">
@@ -108,3 +79,26 @@
 		</div>
 	</body>
 </html>
+
+<?php 
+	
+function login_venue_form() {
+	$redirect = ( is_ssl() ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+	?>
+		<form name="loginform" id="loginform" action="<?php echo esc_url( site_url( 'wp-login.php', 'login_post' ) ) ?>" method="post">
+				<div class="form-group row">
+						<label class="user_login" for="user_login">Username or Email:</label>
+						<input type="text" name="log" id="user_login" class="input form-control shadow-lg" value="" size="20" autofocus placeholder="Enter username or email" required/>
+				</div>
+				<div class="form-group row">
+						<label class="user_pass" for="user_pass">Password:</label>
+						<input type="password" name="pwd" id="user_pass" class="input form-control shadow-lg" value="" size="20" placeholder="Enter your password" required/>
+				</div>
+				<div class="form-group row login-submit d-flex justify-content-center">
+						<input type="submit" name="wp-submit" id="wp-submit" class="btn button-primary py-2 px-5" value="Log In" />
+						<input type="hidden" name="redirect_to" value="" />
+				</div>
+				<input type="hidden" name="redirect_to" value="<?php echo esc_url( $redirect ) ?>" />
+		</form>
+	<?php
+}
