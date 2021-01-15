a25def027e400c6040d7f5f74438cbd0df9a11a4
codeHysteria28 <graphicglobal67@gmail.com>
2021-01-15 09:42:51 +0000

    Added stylesheet for venue-login template to the venue-head partial. For the venue-login replaced the <head> tags with actual venue-head partial. Removed custom code from left,right columns and applied correct bootstrap classes to correctly work with resposivity.

--- page-templates/thetaste-venue-login.php
+++ page-templates/thetaste-venue-login.php
@@ -10,25 +10,8 @@
  * 9/18/2020
  * 
  */
+require_once TASTE_PLUGIN_PATH.'page-templates/partials/venue-head.php';
 ?>
-<!DOCTYPE HTML>
-
-<html>
-
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<meta name="viewport" content="width=device-width" />
-		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-		<link rel="stylesheet" href="<?php echo TASTE_PLUGIN_URL ?>assets/css/font-awesome.min.css">
-		<link rel="preconnect" href="https://fonts.gstatic.com">
-		<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
-		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-		<script	script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-		<script type="text/javascript" src= "<?php echo TASTE_PLUGIN_INCLUDES_URL ?>/js/thetaste-venue-login.js"></script>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
-		<link rel="stylesheet" href="<?php echo TASTE_PLUGIN_INCLUDES_URL ?>/style/css/thetaste-venue-login.css">
-		<title><?php _e('Campaign Login'); ?></title>
-	</head>
 	<body>
     <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">
@@ -51,31 +34,30 @@
             </ul>
         </div>
     </nav>
-		<div class="main_wrapper">
-			<div id="left">
-				<div id="thetaste-logo-link" class="text-center animate__animated animate__bounceInDown">
-<!--					<a href="--><?php //echo get_site_url() ?><!--">-->
-<!--						<img class="img-fluid" src="--><?php //echo get_site_url() ?><!--/wp-content/uploads/2017/12/thetaste-site-homepage-logo5.png">-->
-<!--					</a>-->
-					<h1 class="heading">WELCOME TO IRELAND’S AWARD WINNING</h1>
-					<h2 class="heading2">FOOD, DRINK & TRAVEL DIGITAL MAGAZINE</h2>
-				</div>
-			</div>
-			<div id="right">
-				<div class="bg-white shadow-lg col-sm-8 login_div text-center">
-				<i class="fas fa-user"></i>
-					<?php
-						login_venue_form();
-						if (get_query_var('login') === 'failed') {
-							?>
-							<div class="login-error">
-								<h3>Invalid Login.  Please try again.</h3>
-							</div>
-							<?php
-						}
-					?>
-				</div>
-			</div>
+		<div class="container-fluid h-100">
+            <div class="row h-100">
+                <div class="col-xl-4" id="left">
+                    <div id="thetaste-logo-link" class="text-center">
+                        <h1 class="heading">WELCOME TO IRELAND’S AWARD WINNING</h1>
+                        <h2 class="heading2">FOOD, DRINK & TRAVEL DIGITAL MAGAZINE</h2>
+                    </div>
+                </div>
+                <div class="col-xl-8" id="right">
+                    <div class="col-md-8 col-xl-9 bg-white shadow-lg mt-3 mb-3 mt-sm-3 login_div text-center">
+                        <i class="fas fa-user"></i>
+                        <?php
+                        login_venue_form();
+                        if (get_query_var('login') === 'failed') {
+                            ?>
+                            <div class="login-error">
+                                <h3>Invalid Login.  Please try again.</h3>
+                            </div>
+                            <?php
+                        }
+                        ?>
+                    </div>
+                </div>
+            </div>
 		</div>
 	</body>
 </html>
