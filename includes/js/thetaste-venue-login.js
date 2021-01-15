9eec7fb16aca5ea6a04483f43c2ac6c8cacb536a
Ron Boutilier <ronbout@yahoo.com>
2021-01-14 19:00:57 -0600

    Fixed bug with login javascript.  When text was pasted into one of the fields, it was not running the code to verify that the fields are non-empty before enabling the submit button.  Added the 'paste' event to cover that circumstance.

--- includes/js/thetaste-venue-login.js
+++ includes/js/thetaste-venue-login.js
@@ -17,19 +17,31 @@ jQuery(document).ready(function () {
 	let $submitBtn = jQuery("#wp-submit");
 	$submitBtn.addClass("btn btn-primary");
 	$submitBtn.removeClass("button button-primary");
-	setLoginSubmitability($userName, $userPass, $submitBtn);
+	setLoginSubmitability($userName.val(), $userPass.val(), $submitBtn);
 	$userName.keyup(function () {
-		setLoginSubmitability($userName, $userPass, $submitBtn);
+		setLoginSubmitability($userName.val(), $userPass.val(), $submitBtn);
+	});
+	$userName.on("paste", function (e) {
+		let name = (e.originalEvent.clipboardData || window.clipboardData).getData(
+			"text"
+		);
+		setLoginSubmitability(name, $userPass.val(), $submitBtn);
 	});
 	$userPass.keyup(function () {
-		setLoginSubmitability($userName, $userPass, $submitBtn);
+		setLoginSubmitability($userName.val(), $userPass.val(), $submitBtn);
+	});
+	$userPass.on("paste", function (e) {
+		let pass = (e.originalEvent.clipboardData || window.clipboardData).getData(
+			"text"
+		);
+		setLoginSubmitability($userName.val(), pass, $submitBtn);
 	});
 });
 
 const setLoginSubmitability = ($userName, $userPass, $submitBtn) => {
 	// check that neither login field is empty
 	// otherwise, disable the submit button
-	if (!$userName.val() || !$userPass.val()) {
+	if (!$userName || !$userPass) {
 		$submitBtn.prop("disabled", true);
 	} else {
 		$submitBtn.prop("disabled", false);
