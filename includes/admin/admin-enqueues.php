16aa0c381402bd0f4eada6233a169e0c01b92751
Ronbout <ronbout@yahoo.com>
2022-03-26 12:35:42 -0500

    Make the order metabox for Venue Transactions more presentable by using an HTML Table, displaying the product title, and showing the appropriate transaction date (either payment or redemption).  Also, fixed a bug with the admin css file not being enqueued properly due to an incorrect directory structure in the enqueue command.

--- includes/admin/admin-enqueues.php
+++ includes/admin/admin-enqueues.php
@@ -8,7 +8,7 @@
 	function taste_venue_load_admin_resources($page) {
 
 		// if (in_array($page, array("user-new-php", "user-edit-php", "product_page_venue-assign-products"))) {
-			wp_enqueue_style( 'taste-admin-css', TASTE_PLUGIN_INCLUDES_URL."/css/thetaste-admin.css" );
+			wp_enqueue_style( 'taste-admin-css', TASTE_PLUGIN_INCLUDES_URL."/style/css/thetaste-admin.css" );
 			$dep_array = ("product_page_venue-assign-products" === $page) ? array('jquery-ui-autocomplete') : array();
 			wp_enqueue_script( 'taste-admin-js', TASTE_PLUGIN_INCLUDES_URL . '/js/thetaste-admin.js', $dep_array, false, true);
 			wp_enqueue_script( 'taste-venue-select-js', TASTE_PLUGIN_INCLUDES_URL . '/js/thetaste-venue-select.js', $dep_array, false, true);
