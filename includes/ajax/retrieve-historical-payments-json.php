2e83dff69e98aa3e7b515a33e207fc833e8a20b0
Ronbout <ronbout@yahoo.com>
2022-04-03 08:43:21 -0500

    Fix bug with Historical PBO where the calculation for needed order count was incorrectly referencing the previous Historical PBO order count for that Product.  It is a two dimensional array based on product id.  Forget the reference to product is.   Also changed the calculation for needed to count to use the ceil() operator instead of round().  This will make sure that an order that has only been partially paid is still marked so that it won't be paid in PBO, resulting in an overpayment.

--- includes/ajax/retrieve-historical-payments-json.php
+++ includes/ajax/retrieve-historical-payments-json.php
@@ -95,9 +95,13 @@ function calc_needed_orders($orig_payment_rows, $historical_payment_rows) {
 	foreach ($orig_payment_rows as $prod_id => $pay_row) {
 		$net_payable_per_order = round(calc_net_payable($pay_row['price'], $pay_row['vat_val'], $pay_row['comm_val'], 1), 2);
 		$payment_amount = $pay_row['product_amount'];
-		$historical_order_cnt = (int) round($payment_amount / $net_payable_per_order);
+		// decided that ceiling is better than round as it prevents possibility of overpayment
+		// if payments don't match order net payable's evenly, would rather underpay and have to
+		// make an extra adjustment payment  than overpay and risk never getting that money back.
+		// $historical_order_cnt = (int) round($payment_amount / $net_payable_per_order);
+		$historical_order_cnt = (int) ceil($payment_amount / $net_payable_per_order);
 
-		$prev_historical_order_cnt = isset($historical_payment_rows[$prod_id]) ? $historical_payment_rows['order_cnt'] : 0;
+		$prev_historical_order_cnt = isset($historical_payment_rows[$prod_id]) ? $historical_payment_rows[$prod_id]['order_cnt'] : 0;
 
 		$needed_historical_order_cnt = $historical_order_cnt - $prev_historical_order_cnt;
 
