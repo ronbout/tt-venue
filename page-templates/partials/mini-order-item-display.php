4b3f4834d87872dbeecb2efd556929f937e707b6
Ronbout <ronbout@yahoo.com>
2022-04-14 18:03:55 -0500

    Added the code to organize the Order info returned in the mini order page into an array by order item and an array by venue id.  Then, if the order item id belongs to the logged in venue, the information is available.  This commit just tests the logic and the display is a var_dump of the row.  If the current order item does not belong to the venue, but other items on that order do, that information is displayed to the venue.  Again, this is just a logic test and the order item display is just a var_dump.  This latter situation is likely an instance of the venue accidentally scanning the wrong QR code.  The next step is to focus on the display and UI, including a Redeem button.

--- page-templates/partials/mini-order-item-display.php
+++ page-templates/partials/mini-order-item-display.php
@@ -16,7 +16,6 @@
  */
 defined('ABSPATH') or die('Direct script access disallowed.');
 
-
 function mini_order_item_display($order_item_id, $venue_id, $venue_name) {
 
 
@@ -26,8 +25,10 @@ function mini_order_item_display($order_item_id, $venue_id, $venue_name) {
   <h2>Order Item Id <?php echo $order_item_id ?></h2>
 
   <?php
-    $order_item_rows = retrieve_order($order_item_id)['rows_by_item'];
-    $venue_item_rows = retrieve_order($order_item_id)['rows_by_venue'];
+    
+    $order_db_info = retrieve_order($order_item_id);
+    $order_item_rows = $order_db_info['rows_by_item'];
+    $venue_item_rows = $order_db_info['rows_by_venue'];
 
     if (!isset($venue_item_rows[$venue_id])) {
       ?>
@@ -35,36 +36,58 @@ function mini_order_item_display($order_item_id, $venue_id, $venue_name) {
           This is not a valid Order for <?php echo $venue_name ?>
         </h2>
       <?php
+    } elseif ($order_item_rows[$order_item_id]['venue_id'] == $venue_id) {
+      echo "Here is your Order Item Info: <br>";
+      var_dump($order_item_rows[$order_item_id]);
+    } else {
+      echo "The Order Item you scanned is not a ", $venue_name, "Campaign<br>";
+      $item_count = count($venue_item_rows[$venue_id]);
+      $msg_item = $item_count > 1 ? 'But these Items on that Order are' 
+            : 'But this Item on that Order is';
+      echo $msg_item ."  <br>";
+      var_dump($venue_item_rows[$venue_id]);
     }
 
   $ret_disp = ob_get_clean();
   return $ret_disp;
  }
 
- function retrieve_order($order_item_id) {
-    global $wpdb;
+function retrieve_order($order_item_id) {
+  global $wpdb;
 
-    $sql = "
-      SELECT plook_o.order_id, plook_o.order_item_id, plook_o.product_id, plook_o.product_qty,
-        plook_o.date_created AS order_date,	ord_p.post_status AS order_status, prod_p.post_title,
-        ven.venue_id, ven.name AS venue_name
-      FROM {$wpdb->prefix}wc_order_product_lookup plook_oi
-      JOIN {$wpdb->prefix}wc_order_product_lookup plook_o ON plook_o.order_id = plook_oi.order_id
-      JOIN {$wpdb->prefix}posts prod_p ON prod_p.ID = plook_o.product_id
-      JOIN {$wpdb->prefix}taste_venue_products vprods ON vprods.product_id = plook_o.product_id
-      JOIN {$wpdb->prefix}taste_venue ven ON ven.venue_id = vprods.venue_id
-      JOIN {$wpdb->prefix}posts ord_p ON ord_p.ID = plook_oi.order_id
-      WHERE plook_oi.order_item_id = %d
-    ";
+  $sql = "
+    SELECT plook_o.order_id, plook_o.order_item_id, plook_o.product_id, plook_o.product_qty,
+      plook_o.date_created AS order_date,	ord_p.post_status AS order_status, prod_p.post_title,
+      ven.venue_id, ven.name AS venue_name
+    FROM {$wpdb->prefix}wc_order_product_lookup plook_oi
+    JOIN {$wpdb->prefix}wc_order_product_lookup plook_o ON plook_o.order_id = plook_oi.order_id
+    JOIN {$wpdb->prefix}posts prod_p ON prod_p.ID = plook_o.product_id
+    JOIN {$wpdb->prefix}taste_venue_products vprods ON vprods.product_id = plook_o.product_id
+    JOIN {$wpdb->prefix}taste_venue ven ON ven.venue_id = vprods.venue_id
+    JOIN {$wpdb->prefix}posts ord_p ON ord_p.ID = plook_oi.order_id
+    WHERE plook_oi.order_item_id = %d
+  ";
 
-    $orig_item_rows = $wpdb->get_results($wpdb->prepare($sql, $order_item_id), ARRAY_A); 
-	  $order_item_rows = array_column($orig_item_rows, null, 'order_item_id');
-	  $venue_item_rows = array_column($orig_item_rows, null, 'venue_id');
+  $orig_item_rows = $wpdb->get_results($wpdb->prepare($sql, $order_item_id), ARRAY_A); 
+  $order_item_rows = array_column($orig_item_rows, null, 'order_item_id');
+  $venue_item_rows = build_venue_rows($orig_item_rows);
 
-    return array(
-      'rows_by_item' => $order_item_rows,
-      'rows_by_venue' => $venue_item_rows,
-    );
- }
+  return array(
+    'rows_by_item' => $order_item_rows,
+    'rows_by_venue' => $venue_item_rows,
+  );
+}
+
+function build_venue_rows($order_items) {
+  $venue_item_rows = [];
+  foreach ($order_items as $order_item) {
+    $venue_id = $order_item['venue_id'];
+    if (!isset($venue_item_rows[$venue_id])) {
+      $venue_item_rows[$venue_id] = [];
+    }
+    $venue_item_rows[$venue_id][] = $order_item;
+  }
+  return $venue_item_rows;
+}
 
  
