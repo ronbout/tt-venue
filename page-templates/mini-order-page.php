bb39faaa05e221ac3ca6c067f23bd638aed2052c
Ronbout <ronbout@yahoo.com>
2022-04-17 14:13:30 -0500

    Added code to display a redeemable/not redeemable msg even if the user is not logged in.  That needs more styling and a login button.  Added an order card which displays the basics of an order and a redeem button.  The javascript and hidden data values for the redeem button to work are not yet implemented.  Also, the ability to show other orders by the same venue in case the wrong item was scanned is not yet implemented, although the logic is in place.   It was done with var_dumps and I just have to replace those with order card display code.  I just wanted to reach a stopping point for this commit.

--- page-templates/mini-order-page.php
+++ page-templates/mini-order-page.php
@@ -17,7 +17,10 @@ if (!$order_item_id) {
 }
 
 if ( !is_user_logged_in()) {
-	require_once TASTE_PLUGIN_PATH.'page-templates/thetaste-venue-login.php';
+	// if the user is not logged in, we will still give them a quick 
+	// notice of Redeemable / Not Redeemable and the Venue Name so they
+	// can quickly determine whether it is legitimate
+	non_user_display($order_item_id);
 	die();
 } else {
 	$user_info = get_user_venue_info();
@@ -79,8 +82,9 @@ $venue_table = $wpdb->prefix."taste_venue";
 $navbar_get = $admin ? "venue-id=$venue_id" : "";
 $nav_links = venue_navbar_standard_links($user_info['use_new_campaign'], $user_info['venue_voucher_page'], $admin, $navbar_get);
 ?>
+
 <body>
-<?php
+  <?php
 if (!$venue_id) {
 	// display form to select Venue as user if admin w/o a venue selected
 	display_venue_select(true, 0, true, get_page_link());
@@ -90,24 +94,85 @@ if (!$venue_id) {
 
 ?>
 
-
-<?php venue_navbar($nav_links); ?>
-<div class="container" id="main_wrapper">
-	<div class="row pb-4">
-    <div class="col-sm-12 col-xl-12 dashboard_grid_cols d-flex align-items-center flex-column">
-        <h2 class="dashboard_heading mt-5 font-weight-bold text-left">Order Item Info for <?php echo $venue_name; ?> </h2>
+  <?php venue_navbar($nav_links); ?>
+  <div class="container" id="main_wrapper">
+    <div class="row pb-4">
+      <div class="col-sm-12 col-xl-12 dashboard_grid_cols d-flex align-items-center flex-column">
+        <h2 class="dashboard_heading mt-5 font-weight-bold text-left"><?php echo $venue_name; ?>
+        </h2>
+      </div>
     </div>
-	</div>
-  <div class="order-item-display-container text-center">
-    <?php echo mini_order_item_display($order_item_id, $venue_id, $venue_name) ?>
-  </div>  
-  <br><br>
-	<div class="text-center">
-		<b>IRELAND’S AWARD WINNING FOOD, DRINK & TRAVEL DIGITAL MAGAZINE</b>
-		<br><br>
-		<span style="font-size:12px;">19.6M READERS WORLDWIDE <b>|</b> 10K ARTICLES <b>|</b> €10M GENERATED FOR THE IRISH HOSPITALITY INDUSTRY <b>|</b> 726K REGISTERED MEMBERS <b>|</b> 200K+ TASTE EXPERIENCES SOLD <b>|</b> 300K SOCIAL MEDIA FOLLOWERS <b>|</b> WINNER OF BEST DIGITAL FOOD MAGAZINE IN THE WORLD <b>|</b> WINNER OF OUTSTANDING SMALL BUSINESS IN IRELAND</span>
-	</div>
-</div>
-<script type="text/javascript" src= "<?php echo TASTE_PLUGIN_INCLUDES_URL ?>/js/thetaste-mini-order.js"></script>
+    <div class="order-item-display-container text-center">
+      <?php echo get_mini_order_item_display($order_item_id, $venue_id, $venue_name) ?>
+    </div>
+    <br><br>
+    <div class="text-center">
+      <b>IRELAND’S AWARD WINNING FOOD, DRINK & TRAVEL DIGITAL MAGAZINE</b>
+      <br><br>
+      <span style="font-size:12px;">19.6M READERS WORLDWIDE <b>|</b> 10K ARTICLES <b>|</b> €10M GENERATED FOR THE IRISH
+        HOSPITALITY INDUSTRY <b>|</b> 726K REGISTERED MEMBERS <b>|</b> 200K+ TASTE EXPERIENCES SOLD <b>|</b> 300K SOCIAL
+        MEDIA FOLLOWERS <b>|</b> WINNER OF BEST DIGITAL FOOD MAGAZINE IN THE WORLD <b>|</b> WINNER OF OUTSTANDING SMALL
+        BUSINESS IN IRELAND</span>
+    </div>
+  </div>
+  <script type="text/javascript" src="<?php echo TASTE_PLUGIN_INCLUDES_URL ?>/js/thetaste-mini-order.js"></script>
 </body>
+
 </html>
+
+
+<?php 
+function non_user_display($order_item_id) {
+	$order_item_info = get_order_item_redeemable($order_item_id);
+
+	if (!$order_item_info) {
+		?>
+<h1>
+  <strong>NOT</strong> Redeemable
+</h1>
+<?php
+	} else {
+		?>
+<h1>
+  Redeemable by <?php echo $order_item_info['venue_name'] ?>
+</h1>
+<?php
+	}
+}
+
+function get_order_item_redeemable($order_item_id) {
+	global $wpdb;
+
+	$sql = "
+			SELECT plook.order_id, plook.product_id, plook.product_qty,
+				plook.date_created AS order_date,	ord_p.post_status AS order_status, prod_p.post_title AS prod_desc,
+				ven.venue_id, ven.name AS venue_name, wcoi.downloaded AS redeemed
+			FROM {$wpdb->prefix}wc_order_product_lookup plook
+				JOIN {$wpdb->prefix}posts prod_p ON prod_p.ID = plook.product_id
+				JOIN {$wpdb->prefix}taste_venue_products vprods ON vprods.product_id = plook.product_id
+				JOIN {$wpdb->prefix}taste_venue ven ON ven.venue_id = vprods.venue_id
+				JOIN {$wpdb->prefix}posts ord_p ON ord_p.ID = plook.order_id
+				LEFT JOIN {$wpdb->prefix}woocommerce_order_items wcoi ON wcoi.order_item_id = plook.order_item_id
+			WHERE plook.order_item_id = 174493
+	";
+	
+  $order_item_row = $wpdb->get_results($wpdb->prepare($sql, $order_item_id), ARRAY_A); 
+
+	if (!$order_item_row || !count($order_item_row)) {
+		return false;
+	}
+
+	/***
+	 * 
+	 * will have to have a more robust test of status to 
+	 * deal with partial refunds, whether cash refund or 
+	 * store credit.  Cash refunds should be easier IF(!)
+	 * our people are attaching to refunds to an item.
+	 * 
+	 */
+	if ('wc-completed' != $order_item_row[0]['order_status'] || '1' == $order_item_row[0]['redeemed']) {
+		return false;
+	}
+
+	return $order_item_row[0];
+}
--- page-templates/partials/mini-order-item-display.php
+++ page-templates/partials/mini-order-item-display.php
@@ -16,38 +16,53 @@
  */
 defined('ABSPATH') or die('Direct script access disallowed.');
 
-function mini_order_item_display($order_item_id, $venue_id, $venue_name) {
+function get_mini_order_item_display($order_item_id, $venue_id, $venue_name) {
 
 
   ob_start();
   ?>
 
-  <h2>Order Item Id <?php echo $order_item_id ?></h2>
+<h2>Order Item Id <?php echo $order_item_id ?></h2>
 
+<div class="container">
   <?php
-    
     $order_db_info = retrieve_order($order_item_id);
     $order_item_rows = $order_db_info['rows_by_item'];
     $venue_item_rows = $order_db_info['rows_by_venue'];
 
     if (!isset($venue_item_rows[$venue_id])) {
       ?>
-        <h2 class="text-danger font-weight-bold">
-          This is not a valid Order for <?php echo $venue_name ?>
-        </h2>
-      <?php
+  <h2 class="text-danger font-weight-bold">
+    This is not a valid Order for <?php echo $venue_name ?>
+  </h2>
+  <?php
     } elseif ($order_item_rows[$order_item_id]['venue_id'] == $venue_id) {
-      echo "Here is your Order Item Info: <br>";
-      var_dump($order_item_rows[$order_item_id]);
+      $product_id = $order_item_rows[$order_item_id]['product_id'];
+      ?>
+  <div class="row">
+    <div class="col-md-4">
+    </div>
+    <div class="col-md-4">
+      <?php echo get_order_item_card($order_item_rows[$order_item_id]) ?>
+    </div>
+    <div class="col-md-4">
+    </div>
+  </div>
+
+  <p> </p>
+
+  <?php
     } else {
       echo "The Order Item you scanned is not a ", $venue_name, "Campaign<br>";
-      $item_count = count($venue_item_rows[$venue_id]);
-      $msg_item = $item_count > 1 ? 'But these Items on that Order are' 
-            : 'But this Item on that Order is';
-      echo $msg_item ."  <br>";
-      var_dump($venue_item_rows[$venue_id]);
+      // $item_count = count($venue_item_rows[$venue_id]);
+      // $msg_item = $item_count > 1 ? 'But these Items on that Order are' 
+      //       : 'But this Item on that Order is';
+      // echo $msg_item ."  <br>";
+      // var_dump($venue_item_rows[$venue_id]);
     }
-
+    ?>
+</div>
+<?php
   $ret_disp = ob_get_clean();
   return $ret_disp;
  }
@@ -57,14 +72,17 @@ function retrieve_order($order_item_id) {
 
   $sql = "
     SELECT plook_o.order_id, plook_o.order_item_id, plook_o.product_id, plook_o.product_qty,
-      plook_o.date_created AS order_date,	ord_p.post_status AS order_status, prod_p.post_title,
-      ven.venue_id, ven.name AS venue_name
+      plook_o.date_created AS order_date,	ord_p.post_status AS order_status, 
+      prod_p.post_title as prod_desc, ven.venue_id, ven.name AS venue_name, poix.payment_id, 
+      wcoi.downloaded AS redeemed
     FROM {$wpdb->prefix}wc_order_product_lookup plook_oi
     JOIN {$wpdb->prefix}wc_order_product_lookup plook_o ON plook_o.order_id = plook_oi.order_id
     JOIN {$wpdb->prefix}posts prod_p ON prod_p.ID = plook_o.product_id
     JOIN {$wpdb->prefix}taste_venue_products vprods ON vprods.product_id = plook_o.product_id
     JOIN {$wpdb->prefix}taste_venue ven ON ven.venue_id = vprods.venue_id
     JOIN {$wpdb->prefix}posts ord_p ON ord_p.ID = plook_oi.order_id
+    LEFT JOIN {$wpdb->prefix}woocommerce_order_items wcoi ON wcoi.order_item_id = plook_o.order_item_id
+    LEFT JOIN {$wpdb->prefix}taste_venue_payment_order_item_xref poix ON poix.order_item_id = plook_o.order_item_id
     WHERE plook_oi.order_item_id = %d
   ";
 
@@ -88,6 +106,35 @@ function build_venue_rows($order_items) {
     $venue_item_rows[$venue_id][] = $order_item;
   }
   return $venue_item_rows;
+}
+
+function get_order_item_card($order_item_info) {
+  $product_id = $order_item_info['product_id'];
+  $prod_desc = $order_item_info['prod_desc'];
+  $order_id = $order_item_info['order_id'];
+  $order_item_id = $order_item_info['order_item_id'];
+  $redeemed = $order_item_info['redeemed'];
+  $status = $order_item_info['order_status'];
+  $payment_id = $order_item_info['payment_id'];
+  $redeem_status = "Redeemable";
+  if ('wc-completed' != $status || '1' == $redeemed['redeemed'] ) {
+		$redeem_status = "<strong>NOT</strong> Redeemable";
+	}
+  ob_start();
+  ?>
+<div class="card">
+  <div class="card-body">
+    <?php echo get_the_post_thumbnail($product_id, array( 200, 200), array('class'=>"card-img-top")) ?>
+    <h5 class="card-title">Order ID: <?php echo $order_id ?></h5>
+    <h6 class="card-subtitle mb-2 text-muted">Item: <?php $prod_desc ?></h6>
+    <p class="card-text">
+      Status: <?php echo $redeem_status ?>
+    </p>
+    <button class="btn btn-success order-redeem-btn or-display or-status-display-unredeemed">Redeem</button>
+  </div>
+</div>
+<?php
+  return ob_get_clean(); 
 }
 
  
