62b25dcdb155b31e5884bf5563ff8acdbb2b32f4
Ronbout <ronbout@yahoo.com>
2022-06-25 01:35:52 -0500

    Improve the accuracy of the redeemable test in the mini order page.  Instead of only relying on the order status, which may not apply to all the items on a multi-item order, it now looks at the individual items in the transaction table.  This was not possible before as the refund/credit algorithm was not robust enough to be reliable in some situations.  That has been improved and is now a more accurate test of redeemability.  Also, cleaned up the logic, esp the order retrieval, to be more modular.

--- page-templates/mini-order-page.php
+++ page-templates/mini-order-page.php
@@ -190,40 +190,101 @@ function non_user_display($order_item_id) {
 function get_order_item_redeemable($order_item_id) {
 	global $wpdb;
 
+	$order_rows = retrieve_order($order_item_id);
+	
+	if (!$order_rows || !count($order_rows)) {
+		return false;
+	}
+	
+  $order_item_row = $order_rows['rows_by_item'][$order_item_id];
+
+
+	return redeemable_ret_val(test_redeemable($order_item_row), $order_item_row);
+
+}
+
+function redeemable_ret_val($redeemable, $order_info) {
+	return array(
+		'redeemable' => $redeemable,
+		'order_item_info' => $order_info,
+	);
+}
+
+function test_redeemable($order_info) {
+		// 1) already redeemed => NO
+		if ( '1' == $order_info['redeemed']) {
+			return false;
+		}
+	
+		// 2) wc-refunded => NO
+		if ('wc-refunded' == $order_info['order_status']) {
+			return false;
+		}
+	
+		// 3) credited
+		if ($order_info['credited'] > 0) {
+			return false;
+		}
+		
+		// 4) refunded w/o refund status (partial refund)
+		if ($order_info['refunded'] > 0) {
+			return false;
+		}
+
+		// on hold for some reason OTHER than credited
+		if ('wc-on-hold' == $order_info['order_status'] && $order_info['credited_order'] == 0) {
+			return false;
+		}
+		
+		return true;
+}
+
+function retrieve_order($order_item_id) {
+  global $wpdb;
+
 	$sql = "
 			SELECT plook.order_id, plook.product_id, plook.product_qty,
-				plook.date_created AS order_date,	ord_p.post_status AS order_status, prod_p.post_title AS prod_desc, plook.order_item_id,
-				ven.venue_id, ven.name AS venue_name, wcoi.downloaded AS redeemed
-			FROM {$wpdb->prefix}wc_order_product_lookup plook
+				plook.date_created AS order_date,	ord_p.post_status AS order_status, prod_p.post_title AS prod_desc, plook.order_item_id, poix.payment_id,
+				ven.venue_id, ven.name AS venue_name, wcoi.downloaded AS redeemed,
+				oit_cred.trans_amount AS credited, oit_ref.trans_amount AS refunded,
+				(
+					SELECT COUNT(*) 
+					FROM {$wpdb->prefix}taste_order_transactions oit_o 
+					WHERE oit_o.order_id = plook.order_id AND oit_o.trans_type = 'Taste Credit'
+				) AS credited_order
+			FROM {$wpdb->prefix}wc_order_product_lookup plook_oi
+				JOIN {$wpdb->prefix}wc_order_product_lookup plook ON plook.order_id = plook_oi.order_id
 				JOIN {$wpdb->prefix}posts prod_p ON prod_p.ID = plook.product_id
 				JOIN {$wpdb->prefix}taste_venue_products vprods ON vprods.product_id = plook.product_id
 				JOIN {$wpdb->prefix}taste_venue ven ON ven.venue_id = vprods.venue_id
 				JOIN {$wpdb->prefix}posts ord_p ON ord_p.ID = plook.order_id
+				LEFT JOIN {$wpdb->prefix}taste_venue_payment_order_item_xref poix ON poix.order_item_id = plook.order_item_id
 				LEFT JOIN {$wpdb->prefix}woocommerce_order_items wcoi ON wcoi.order_item_id = plook.order_item_id
-			WHERE plook.order_item_id = %d
+				LEFT JOIN {$wpdb->prefix}taste_order_transactions oit_cred ON oit_cred.order_item_id = plook.order_item_id
+					AND oit_cred.trans_type = 'Taste Credit'
+				LEFT JOIN {$wpdb->prefix}taste_order_transactions oit_ref ON oit_ref.order_item_id = plook.order_item_id
+					AND oit_ref.trans_type = 'Refund'
+			WHERE plook_oi.order_item_id = %d
 	";
-	
-  $order_item_row = $wpdb->get_results($wpdb->prepare($sql, $order_item_id), ARRAY_A); 
 
-	if (!$order_item_row || !count($order_item_row)) {
-		return false;
-	}
+  $orig_item_rows = $wpdb->get_results($wpdb->prepare($sql, $order_item_id), ARRAY_A); 
+  $order_item_rows = array_column($orig_item_rows, null, 'order_item_id');
+  $venue_item_rows = build_venue_rows($orig_item_rows);
 
-	/***
-	 * 
-	 * will have to have a more robust test of status to 
-	 * deal with partial refunds, whether cash refund or 
-	 * store credit.  Cash refunds should be easier IF(!)
-	 * our people are attaching to refunds to an item.
-	 * 
-	 */
-	$redeemable = true;
-	if ('wc-completed' != $order_item_row[0]['order_status'] || '1' == $order_item_row[0]['redeemed']) {
-		$redeemable = false;
-	}
+  return array(
+    'rows_by_item' => $order_item_rows,
+    'rows_by_venue' => $venue_item_rows,
+  );
+}
 
-	return array(
-		'redeemable' => $redeemable,
-		'order_item_info' => $order_item_row[0],
-	);
+function build_venue_rows($order_items) {
+  $venue_item_rows = [];
+  foreach ($order_items as $order_item) {
+    $venue_id = $order_item['venue_id'];
+    if (!isset($venue_item_rows[$venue_id])) {
+      $venue_item_rows[$venue_id] = [];
+    }
+    $venue_item_rows[$venue_id][] = $order_item;
+  }
+  return $venue_item_rows;
 }
--- page-templates/partials/mini-order-item-display.php
+++ page-templates/partials/mini-order-item-display.php
@@ -84,46 +84,7 @@ function get_mini_order_item_display($order_item_id, $venue_id, $venue_name) {
   return $ret_disp;
  }
 
-function retrieve_order($order_item_id) {
-  global $wpdb;
 
-  $sql = "
-    SELECT plook_o.order_id, plook_o.order_item_id, plook_o.product_id, plook_o.product_qty,
-      plook_o.date_created AS order_date,	ord_p.post_status AS order_status, 
-      prod_p.post_title as prod_desc, ven.venue_id, ven.name AS venue_name, poix.payment_id, 
-      wcoi.downloaded AS redeemed
-    FROM {$wpdb->prefix}wc_order_product_lookup plook_oi
-    JOIN {$wpdb->prefix}wc_order_product_lookup plook_o ON plook_o.order_id = plook_oi.order_id
-    JOIN {$wpdb->prefix}posts prod_p ON prod_p.ID = plook_o.product_id
-    JOIN {$wpdb->prefix}taste_venue_products vprods ON vprods.product_id = plook_o.product_id
-    JOIN {$wpdb->prefix}taste_venue ven ON ven.venue_id = vprods.venue_id
-    JOIN {$wpdb->prefix}posts ord_p ON ord_p.ID = plook_oi.order_id
-    LEFT JOIN {$wpdb->prefix}woocommerce_order_items wcoi ON wcoi.order_item_id = plook_o.order_item_id
-    LEFT JOIN {$wpdb->prefix}taste_venue_payment_order_item_xref poix ON poix.order_item_id = plook_o.order_item_id
-    WHERE plook_oi.order_item_id = %d
-  ";
-
-  $orig_item_rows = $wpdb->get_results($wpdb->prepare($sql, $order_item_id), ARRAY_A); 
-  $order_item_rows = array_column($orig_item_rows, null, 'order_item_id');
-  $venue_item_rows = build_venue_rows($orig_item_rows);
-
-  return array(
-    'rows_by_item' => $order_item_rows,
-    'rows_by_venue' => $venue_item_rows,
-  );
-}
-
-function build_venue_rows($order_items) {
-  $venue_item_rows = [];
-  foreach ($order_items as $order_item) {
-    $venue_id = $order_item['venue_id'];
-    if (!isset($venue_item_rows[$venue_id])) {
-      $venue_item_rows[$venue_id] = [];
-    }
-    $venue_item_rows[$venue_id][] = $order_item;
-  }
-  return $venue_item_rows;
-}
 
 function get_order_item_card($order_item_info, $disp_btn=true) {
   $product_id = $order_item_info['product_id'];
@@ -133,10 +94,8 @@ function get_order_item_card($order_item_info, $disp_btn=true) {
   $redeemed = $order_item_info['redeemed'];
   $status = $order_item_info['order_status'];
   //$payment_id = $order_item_info['payment_id'];
-  $redeemable = true;
-  if ('wc-completed' != $status || '1' == $redeemed['redeemed'] ) {
-		$redeemable = false;
-	}
+
+  $redeemable = test_redeemable($order_item_info);
   if ($redeemable) {
     $redeem_status = "Redeemable";
     $status_color = "text-primary";
