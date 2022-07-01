2662d6087dfcc7ef868eeef7a34b9e5f1b0f4399
Ronbout <ronbout@yahoo.com>
2022-07-01 07:28:04 -0500

    Added a js validation to the product entry/update page so that it cannot be saved if a venue has not been selected.

--- includes/js/thetaste-admin.js
+++ includes/js/thetaste-admin.js
@@ -1,120 +1,120 @@
 jQuery(document).ready(function () {
-	let $documentBody = jQuery("body");
-	userForms($documentBody);
-	editPostVenues($documentBody);
-	if (typeof products != "undefined") {
-		// console.log("products ", products);
-		let selectedProds = [];
-		loadSelectProducts(selectedProds);
-	} else {
-		// console.log("no products");
-	}
+  let $documentBody = jQuery("body");
+  userForms($documentBody);
+  editPostVenues($documentBody);
+  validateProduct($documentBody);
+  if (typeof products != "undefined") {
+    // console.log("products ", products);
+    let selectedProds = [];
+    loadSelectProducts(selectedProds);
+  } else {
+    // console.log("no products");
+  }
 });
 
 const userForms = ($documentBody) => {
-	if (
-		$documentBody.hasClass("user-new-php") ||
-		$documentBody.hasClass("user-edit-php")
-	) {
-		let $roleSelect = jQuery("#role");
-		$roleSelect.length &&
-			$roleSelect.change(function () {
-				let selectRole = this.value;
-				if ("venue" === selectRole) {
-					jQuery("#user-venue-fields").css("display", "block");
-				} else {
-					jQuery("#user-venue-fields").css("display", "none");
-				}
-			});
-	}
+  if (
+    $documentBody.hasClass("user-new-php") ||
+    $documentBody.hasClass("user-edit-php")
+  ) {
+    let $roleSelect = jQuery("#role");
+    $roleSelect.length &&
+      $roleSelect.change(function () {
+        let selectRole = this.value;
+        if ("venue" === selectRole) {
+          jQuery("#user-venue-fields").css("display", "block");
+        } else {
+          jQuery("#user-venue-fields").css("display", "none");
+        }
+      });
+  }
 };
 
 const editPostVenues = ($documentBody) => {
-	if (
-		($documentBody.hasClass("post-php") ||
-			$documentBody.hasClass("post-new-php") ||
-			$documentBody.hasClass("edit-php")) &&
-		$documentBody.hasClass("post-type-post")
-	) {
-		// postVenuesList is available
-		console.log("postVenuesList: ", postVenuesList);
-		buildVenueListsForPostAssign(postVenuesList);
-		// setup change of the venue select to add to the venue lists
-		$venueSelect = jQuery("#venue-select");
-		$venueSelect.change(() => {
-			let venueId = Number.parseInt($venueSelect.val());
-			let fnd = postVenuesList.findIndex((venInfo) => {
-				return Number.parseInt(venInfo.venueId) === venueId;
-			});
-			if (fnd !== -1) return;
-			let venueSelected = {
-				venueId,
-				name: jQuery("#venue-select option:selected").text().trim(),
-			};
-			postVenuesList.push(venueSelected);
+  if (
+    ($documentBody.hasClass("post-php") ||
+      $documentBody.hasClass("post-new-php") ||
+      $documentBody.hasClass("edit-php")) &&
+    $documentBody.hasClass("post-type-post")
+  ) {
+    // postVenuesList is available
+    console.log("postVenuesList: ", postVenuesList);
+    buildVenueListsForPostAssign(postVenuesList);
+    // setup change of the venue select to add to the venue lists
+    $venueSelect.change(() => {
+      let venueId = Number.parseInt($venueSelect.val());
+      let fnd = postVenuesList.findIndex((venInfo) => {
+        return Number.parseInt(venInfo.venueId) === venueId;
+      });
+      if (fnd !== -1) return;
+      let venueSelected = {
+        venueId,
+        name: jQuery("#venue-select option:selected").text().trim(),
+      };
+      postVenuesList.push(venueSelected);
 
-			buildVenueListsForPostAssign(postVenuesList);
-		});
-	}
+      buildVenueListsForPostAssign(postVenuesList);
+    });
+  }
 };
 
 const buildVenueListsForPostAssign = (postVenuesList) => {
-	let venueInputList = postVenuesList.reduce((venList, venInfo) => {
-		venList += venList ? "," : "";
-		venList += venInfo.venueId;
-		return venList;
-	}, "");
+  let venueInputList = postVenuesList.reduce((venList, venInfo) => {
+    venList += venList ? "," : "";
+    venList += venInfo.venueId;
+    return venList;
+  }, "");
 
-	let venueChipList = postVenuesList.reduce((venList, venInfo) => {
-		venList += `
+  let venueChipList = postVenuesList.reduce((venList, venInfo) => {
+    venList += `
 			<div class="tt-chip">
 				${venInfo.name}
 				<span data-venid="${venInfo.venueId}" class="delete-venue-chip dashicons dashicons-no-alt tt-icon"></span>
 			</div>
 		`;
-		return venList;
-	}, "");
-	jQuery("#post-venue-id-list").val(venueInputList);
-	jQuery("#selected-venues-chips").html(venueChipList);
-	setupVenueChipClicks(postVenuesList);
+    return venList;
+  }, "");
+  jQuery("#post-venue-id-list").val(venueInputList);
+  jQuery("#selected-venues-chips").html(venueChipList);
+  setupVenueChipClicks(postVenuesList);
 };
 
 const setupVenueChipClicks = (postVenuesList) => {
-	jQuery(".delete-venue-chip")
-		.unbind("click")
-		.click(function (e) {
-			e.preventDefault();
-			let venueId = Number.parseInt(jQuery(this).data("venid"));
-			let fnd = postVenuesList.findIndex((venInfo) => {
-				return Number.parseInt(venInfo.venueId) === venueId;
-			});
-			if (fnd !== -1) {
-				postVenuesList.splice(fnd, 1);
-			}
-			buildVenueListsForPostAssign(postVenuesList);
-		});
+  jQuery(".delete-venue-chip")
+    .unbind("click")
+    .click(function (e) {
+      e.preventDefault();
+      let venueId = Number.parseInt(jQuery(this).data("venid"));
+      let fnd = postVenuesList.findIndex((venInfo) => {
+        return Number.parseInt(venInfo.venueId) === venueId;
+      });
+      if (fnd !== -1) {
+        postVenuesList.splice(fnd, 1);
+      }
+      buildVenueListsForPostAssign(postVenuesList);
+    });
 };
 
 const loadSelectProducts = (selectedProds) => {
-	jQuery("#product-input").autocomplete({
-		source: products,
-		select: function (event, ui) {
-			event.preventDefault();
-			// jQuery(this).val("");
-			let ndx = products.findIndex((p) => {
-				return p.value === ui.item.value;
-			});
-			let selected = products.splice(ndx, 1);
-			selectedProds = selectedProds.concat(selected);
-			let selectTbody = buildSelectedRows(selectedProds);
-			jQuery("#select-products-body").html(selectTbody);
-			jQuery("#product-input").autocomplete("option", "source", products);
-			// add click events to freshly created buttons in selected table
-			setupUnSelect(selectedProds, products);
-		},
-	});
+  jQuery("#product-input").autocomplete({
+    source: products,
+    select: function (event, ui) {
+      event.preventDefault();
+      // jQuery(this).val("");
+      let ndx = products.findIndex((p) => {
+        return p.value === ui.item.value;
+      });
+      let selected = products.splice(ndx, 1);
+      selectedProds = selectedProds.concat(selected);
+      let selectTbody = buildSelectedRows(selectedProds);
+      jQuery("#select-products-body").html(selectTbody);
+      jQuery("#product-input").autocomplete("option", "source", products);
+      // add click events to freshly created buttons in selected table
+      setupUnSelect(selectedProds, products);
+    },
+  });
 
-	/*
+  /*
 	jQuery("#assign-products-btn").click(function (e) {
 		// no use for this now...later can use with ajax call
 	});
@@ -122,23 +122,23 @@ const loadSelectProducts = (selectedProds) => {
 };
 
 const setupUnSelect = (selectedProds, products) => {
-	jQuery(".unselect-btn").click(function (e) {
-		e.preventDefault();
-		let ndx = jQuery(this).data("ndx");
-		let unSelected = selectedProds.splice(ndx, 1);
-		products = products.concat(unSelected);
-		let selectTbody = buildSelectedRows(selectedProds);
-		jQuery("#select-products-body").html(selectTbody);
-		jQuery("#product-input").autocomplete("option", "source", products);
-		setupUnSelect(selectedProds, products);
-	});
+  jQuery(".unselect-btn").click(function (e) {
+    e.preventDefault();
+    let ndx = jQuery(this).data("ndx");
+    let unSelected = selectedProds.splice(ndx, 1);
+    products = products.concat(unSelected);
+    let selectTbody = buildSelectedRows(selectedProds);
+    jQuery("#select-products-body").html(selectTbody);
+    jQuery("#product-input").autocomplete("option", "source", products);
+    setupUnSelect(selectedProds, products);
+  });
 };
 
 const buildSelectedRows = (selectedProds) => {
-	let selectTbody = selectedProds.reduce((bodyRows, prod, ndx) => {
-		bodyRows =
-			bodyRows +
-			`
+  let selectTbody = selectedProds.reduce((bodyRows, prod, ndx) => {
+    bodyRows =
+      bodyRows +
+      `
 		<tr>
 		<td>${prod.prodId}</td>
 		<td>${prod.sku}</td>
@@ -151,12 +151,50 @@ const buildSelectedRows = (selectedProds) => {
 		</td>
 	</tr>
 		`;
-		return bodyRows;
-	}, "");
-	if (selectedProds.length) {
-		jQuery("#assign-products-btn").prop("disabled", false);
-	} else {
-		jQuery("#assign-products-btn").prop("disabled", true);
-	}
-	return selectTbody;
+    return bodyRows;
+  }, "");
+  if (selectedProds.length) {
+    jQuery("#assign-products-btn").prop("disabled", false);
+  } else {
+    jQuery("#assign-products-btn").prop("disabled", true);
+  }
+  return selectTbody;
 };
+
+const validateProduct = ($documentBody) => {
+  if ($documentBody.hasClass("post-type-product")) {
+    $venueSelect = jQuery("#venue-select");
+    $publishBtn = jQuery("#publish");
+    $majorActionDiv = jQuery("#major-publishing-actions");
+    $majorActionDiv.after(
+      `<div id='venue-required-msg' class='notice notice-error' >
+				<p>Venue is required</p>
+			</div>`
+    );
+    // Update button should be disabled if no venue selected
+    if ($venueSelect.val() === "0") {
+      disable_prod_save($publishBtn);
+    }
+    $venueSelect.change(() => {
+      if ($venueSelect.val() === "0") {
+        disable_prod_save($publishBtn);
+      } else {
+        enable_prod_save($publishBtn);
+      }
+    });
+  }
+};
+
+function disable_prod_save($publishBtn) {
+  $venueRequiredMsg = jQuery("#venue-required-msg");
+  $publishBtn.attr("disabled", true);
+  $publishBtn.attr("title", "You must select a Venue to Save this Product");
+  $venueRequiredMsg.show();
+}
+
+function enable_prod_save($publishBtn) {
+  $venueRequiredMsg = jQuery("#venue-required-msg");
+  $publishBtn.attr("disabled", false);
+  $publishBtn.attr("title", "");
+  $venueRequiredMsg.hide();
+}
