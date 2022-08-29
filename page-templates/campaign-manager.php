9e9709fd3b1e674c139e8f1828aabd9f2df0dc3c
Ronbout <ronbout@yahoo.com>
2022-08-29 10:34:03 -0500

    Add a default to the get_option call in CM when retreiving the pbo rounding threshold

--- page-templates/campaign-manager.php
+++ page-templates/campaign-manager.php
@@ -88,7 +88,7 @@ $nav_links = venue_navbar_standard_links($user_info['use_new_campaign'], $user_i
 				tasteVenue.ajaxurl = '". admin_url( 'admin-ajax.php' ) . "'
 				tasteVenue.security = '" . wp_create_nonce('taste-venue-nonce') . "'
 				tasteVenue.displayMode = '" . $display_mode . "'
-        tasteVenue.pboThreshold = ". get_option( 'tf_financials_rounding_threshold') . "
+        tasteVenue.pboThreshold = ". get_option( 'tf_financials_rounding_threshold', 0.1) . "
 			</script>
 		";
 	$navbar_get = $admin ? "venue-id=$venue_id" : "";
